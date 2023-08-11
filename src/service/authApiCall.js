81f61315f866833f740c5b9f9ebf14d6c81f4b16
Ekrem <avekremyilmazturk@gmail.com>
2023-08-11 21:50:51 +0300

    invalid hook problem

--- src/service/authApiCall.js
+++ src/service/authApiCall.js
@@ -1,15 +1,20 @@
 import axios from "axios"
 import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify"
+import { useNavigate } from "react-router-dom"
+
 
 export const login = async (userData)=> {
+    const navigate = useNavigate()
     const BASE_URL = "http://13121.fullstack.clarusway.com"
     try {
         const { data } = await axios.post(
           `${BASE_URL}/account/auth/login/`,
           userData
+
         )
-        console.log(data);
+        
         toastSuccessNotify("login islemi basarili")
+        navigate("/stock")
     } catch (error) {
         console.log(error);
     }
