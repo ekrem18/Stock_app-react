12d6044918e9e0864d9cef8489a5b5a52dbc2f5e
Ekrem <avekremyilmazturk@gmail.com>
2023-08-11 21:56:11 +0300

    dşaspatch part

--- src/service/authApiCall.js
+++ src/service/authApiCall.js
@@ -3,20 +3,24 @@ import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify"
 import { useNavigate } from "react-router-dom"
 
 
-export const login = async (userData)=> {
+export const login = async (userData) => {
     const navigate = useNavigate()
+    const dispatch = useDispatch()
+  
     const BASE_URL = "http://13121.fullstack.clarusway.com"
+  
+    dispatch(fetchStart())
     try {
-        const { data } = await axios.post(
-          `${BASE_URL}/account/auth/login/`,
-          userData
-
-        )
-        
-        toastSuccessNotify("login islemi basarili")
-        navigate("/stock")
+      const { data } = await axios.post(
+        `${BASE_URL}/account/auth/login/`,
+        userData
+      )
+      dispatch(loginSuccess(data))
+      toastSuccessNotify("login islemi basarili")
+      navigate("/stock")
     } catch (error) {
-        console.log(error);
+      console.log(error)
+      dispatch(fetchFail())
     }
+  }
 
-}
