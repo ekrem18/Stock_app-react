3e2be8c10b42f60a029ddd4e9fe4e0e7ff124768
Ekrem <avekremyilmazturk@gmail.com>
2023-08-14 13:41:32 +0300

    window.open done

--- src/components/MenuListItems.jsx
+++ src/components/MenuListItems.jsx
@@ -60,7 +60,11 @@ const MenuListItems = () => {
           <ListItem
             key={index}
             disablePadding
-            onClick={() => navigate(item.url)}
+            onClick={() => {
+              item.url.includes("http" || "www")
+                ? window.open(item.url, "_blank")
+                : navigate(item.url);
+            }}
             sx={{
               color: "white",
               "& .MuiSvgIcon-root": { color: "white" },
