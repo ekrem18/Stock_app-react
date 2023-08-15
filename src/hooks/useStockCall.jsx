c68691d13e2e157dc71e34087ef0c81a49612868
Ekrem <avekremyilmazturk@gmail.com>
2023-08-15 15:21:46 +0300

    customHook

--- src/hooks/useStockCall.jsx
+++ src/hooks/useStockCall.jsx
@@ -1,22 +1,19 @@
-import axios from "axios"
 import { fetchFail, fetchStart, getStockSuccess } from "../features/stockSlice"
 import { useDispatch, useSelector } from "react-redux"
 import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify"
+import useAxios from "./useAxios"
 
 const useStockCall = () => {
   
   const { token } = useSelector((state) => state.auth)
   const dispatch = useDispatch()
+  
+  const {axiosWithToken}= useAxios()
 
   const getStockData = async (url) => {
     dispatch(fetchStart())
     try {
-      const { data } = await axios(
-        `${import.meta.env.VITE_BASE_URL}/stock/${url}/`,
-        {
-          headers: { Authorization: `Token ${token}` },
-        }
-      )
+      const { data } = await axiosWithToken(`/stock/${url}/`)
       dispatch(getStockSuccess({ data, url }))
       console.log(data)
     } catch (error) {
