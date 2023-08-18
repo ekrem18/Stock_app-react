c3c915dfc08bc29b61d440c97da9ed14d356735c
Ekrem <avekremyilmazturk@gmail.com>
2023-08-18 22:46:57 +0300

    producttable styling

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -1,64 +1,64 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
+import { useSelector } from 'react-redux';
 
 const columns = [
-  { field: 'id', headerName: '#', width: 90 , headerAlign:"center"},
-  
   {
-    field: 'category',
-    headerName: 'Category',
-    width: 150,
-    // editable: true,
+    field: "id",
+    headerName: "#",
+    headerAlign: "center",
+    align: "center",
+    flex: 0.5,
   },
   {
-    field: 'brand',
-    headerName: 'Brand',
-    type: 'number',
-    width: 110,
-    // editable: true,
+    field: "category",
+    headerName: "Category",
+    flex: 1.5,
+    headerAlign: "center",
+    align: "center",
+    minWidth: 80,
   },
   {
-    field: 'name',
-    headerName: 'Name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    // valueGetter: (params) =>
-    //   `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    field: "brand",
+    headerName: "Brand",
+    flex: 1.5,
+    headerAlign: "center",
+    align: "center",
   },
   {
-    field: 'stock',
-    headerName: 'Stock',
-    width: 150,
-    // editable: true,
+    field: "name",
+    headerName: "Name",
+    flex: 1.5,
+    headerAlign: "center",
+    align: "center",
+  },
+  {
+    field: "stock",
+    headerName: "Stock",
+    type: "number",
+    flex: 1,
+    headerAlign: "center",
+    align: "center",
   },
 
   {
     field: 'actions',
     headerName: 'Actions',
-    width: 150,
+    flex: 1,
     // editable: true,
+    headerAlign:"center",
+    align:"center",
   },
 ];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
 
 export default function ProductTable() {
+  const {products} = useSelector((state)=> state.stock)
   return (
     <Box sx={{ height: 400, width: '100%' }}>
       <DataGrid
-        rows={rows}
+        rows={products}
         columns={columns}
         initialState={{
           pagination: {
