bb738e7d0d216161899e5cf9f78c474c2eb23fdc
Ekrem <avekremyilmazturk@gmail.com>
2023-08-19 12:35:06 +0300

    dleete button

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -1,62 +1,75 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import { DataGrid } from '@mui/x-data-grid';
-import { useSelector } from 'react-redux';
-
-const columns = [
-  {
-    field: "id",
-    headerName: "#",
-    headerAlign: "center",
-    align: "center",
-    flex: 0.5,
-  },
-  {
-    field: "category",
-    headerName: "Category",
-    flex: 1.5,
-    headerAlign: "center",
-    align: "center",
-    minWidth: 80,
-  },
-  {
-    field: "brand",
-    headerName: "Brand",
-    flex: 1.5,
-    headerAlign: "center",
-    align: "center",
-  },
-  {
-    field: "name",
-    headerName: "Name",
-    flex: 1.5,
-    headerAlign: "center",
-    align: "center",
-  },
-  {
-    field: "stock",
-    headerName: "Stock",
-    type: "number",
-    flex: 1,
-    headerAlign: "center",
-    align: "center",
-  },
-
-  {
-    field: 'actions',
-    headerName: 'Actions',
-    flex: 1,
-    // editable: true,
-    headerAlign:"center",
-    align:"center",
-  },
-];
-
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { DataGrid, GridActionsCellItem } from "@mui/x-data-grid";
+import { useSelector } from "react-redux";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import { btnStyle } from "../styles/globalStyles";
+import useStockCall from "../hooks/useStockCall";
 
 export default function ProductTable() {
-  const {products} = useSelector((state)=> state.stock)
+  const { products } = useSelector((state) => state.stock);
+  const { deleteStockData } = useStockCall();
+
+  const columns = [
+    {
+      field: "id",
+      headerName: "#",
+      headerAlign: "center",
+      align: "center",
+      flex: 0.5,
+    },
+    {
+      field: "category",
+      headerName: "Category",
+      flex: 1.5,
+      headerAlign: "center",
+      align: "center",
+      minWidth: 80,
+    },
+    {
+      field: "brand",
+      headerName: "Brand",
+      flex: 1.5,
+      headerAlign: "center",
+      align: "center",
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1.5,
+      headerAlign: "center",
+      align: "center",
+    },
+    {
+      field: "stock",
+      headerName: "Stock",
+      type: "number",
+      flex: 1,
+      headerAlign: "center",
+      align: "center",
+    },
+
+    {
+      field: "actions",
+      type: "actions",
+      headerName: "Actions",
+      flex: 1,
+      // editable: true,
+      headerAlign: "center",
+      align: "center",
+      getActions: (props) => [
+        <GridActionsCellItem
+          icon={<DeleteForeverIcon />}
+          label="Delete"
+          sx={btnStyle}
+          onClick={() => deleteStockData("products", props.id)}
+        />,
+      ],
+    },
+  ];
+
   return (
-    <Box sx={{ height: 400, width: '100%' }}>
+    <Box sx={{ height: 400, width: "100%" }}>
       <DataGrid
         rows={products}
         columns={columns}
