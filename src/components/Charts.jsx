c4e894112d3f8ba9e0ba131b9a0b3d9f7c0ba0f2
Ekrem <avekremyilmazturk@gmail.com>
2023-08-21 15:39:55 +0300

    charts

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,52 +1,56 @@
+import { Grid } from "@mui/material";
 import { Card, Title, LineChart } from "@tremor/react";
 import { useSelector } from "react-redux";
 
-const dataFormatter = (number) => `${Intl.NumberFormat("us").format(number).toString()}`;
+const dataFormatter = (number) =>
+  `${Intl.NumberFormat("us").format(number).toString()}`;
 
 const Charts = () => {
-  const{sales, purchases} = useSelector((state) => state.stock)
-  
-  const salesData = sales?.map((item)=> ({
+  const { sales, purchases } = useSelector((state) => state.stock);
+
+  const salesData = sales?.map((item) => ({
     date: item.createds,
     quantity: item.quantity,
     price: item.price_total,
-  }))
+  }));
 
-  const purchasesData = sales?.map((item)=> ({
+  const purchasesData = sales?.map((item) => ({
     date: item.createds,
     quantity: item.quantity,
     price: item.price_total,
-  }))
-// console.log(s);
-  return(
-  <>
-  <Card >
-    <Title>Total Purchases</Title>
-    <LineChart
-      className="mt-6"
-      data={purchasesData}
-      index="date"
-      categories={["quantity", "price"]}
-      colors={["emerald", "gray"]}
-      valueFormatter={dataFormatter}
-    
-    />
-  </Card>
+  }));
+  // console.log(s);
+  return (
+    <Grid container justifyContent="center" spacing={3} mt={4}>
+      <Grid item xs={12} md={6}>
+        <Card>
+          <Title>Total Purchases</Title>
+          <LineChart
+            className="mt-6"
+            data={purchasesData}
+            index="date"
+            categories={["quantity", "price"]}
+            colors={["emerald", "gray"]}
+            valueFormatter={dataFormatter}
+          />
+        </Card>
+        </Grid>
 
-  <Card >
-    <Title>Total Sales</Title>
-    <LineChart
-      className="mt-6"
-      data={salesData}
-      index="date"
-      categories={["quantity", "price"]}
-      colors={["emerald", "gray"]}
-      valueFormatter={dataFormatter}
-    
-    />
-  </Card>
-  </>
-  )
+        <Grid item xs={12} md={6}>
+        <Card>
+          <Title>Total Sales</Title>
+          <LineChart
+            className="mt-6"
+            data={salesData}
+            index="date"
+            categories={["price"]}
+            colors={["emerald", "gray"]}
+            valueFormatter={dataFormatter}
+          />
+        </Card>
+      </Grid>
+    </Grid>
+  );
 };
 
-export default Charts
+export default Charts;
